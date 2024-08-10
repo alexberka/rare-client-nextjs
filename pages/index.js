39e3a648911903af5b119bd536b401766635ec1f
Yarelis Martin <yarelis0martin@gmail.com>
2024-08-10 12:33:21 -0500

    created the filter post by category drop down button

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,45 @@
 import { useEffect, useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import PostCard from '../components/PostCard';
-import { getPosts } from '../api/postData';
+import { getPosts, getPostsByCategory } from '../api/postData';
+import { getCategories } from '../api/categoryData';
 
 function Home() {
   const [posts, setPosts] = useState([]);
+  const [categories, setcategories] = useState([]);
 
   const getAllPost = () => {
     getPosts().then(setPosts);
   };
 
+  const getAllCategories = () => {
+    getCategories().then(setcategories);
+  };
+
+  const handleCategoryClick = (id) => {
+    getPostsByCategory(id).then(setPosts);
+  };
+
   useEffect(() => {
     getAllPost();
+    getAllCategories();
   }, []);
 
   return (
     <div>
+      <DropdownButton
+        align="end"
+        id="dropdown-item-button"
+        title="Search by Category"
+        className="drop-down-filter"
+      >
+        {categories.map((category) => (
+          <div key={category.id}>
+            <Dropdown.Item as="button" onClick={() => handleCategoryClick(category.id)}>{category.label}</Dropdown.Item>
+          </div>
+        ))}
+      </DropdownButton>
       {posts.map((post) => (
         <PostCard key={post.id} postObj={post} />
       ))}
