f2046138e307297c11fd3b8eba759a308531b868
Yarelis Martin <yarelis0martin@gmail.com>
2024-08-10 13:41:48 -0500

    added all post btn and a result message

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import { Button } from 'react-bootstrap';
 import PostCard from '../components/PostCard';
 import { getPosts, getPostsByCategory, getPostsByUsers } from '../api/postData';
 import { getCategories } from '../api/categoryData';
@@ -8,45 +9,50 @@ import { getUsers } from '../api/userData';
 
 function Home() {
   const [posts, setPosts] = useState([]);
-  const [categoriePosts, setCategoriePosts] = useState([]);
+  const [categoryPosts, setCategoryPosts] = useState([]);
   const [authorPosts, setAuthorPosts] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('');
 
   const getAllPost = () => {
     getPosts().then(setPosts);
+    setActiveFilter('All Posts');
   };
 
-  const getAllCategoriePost = () => {
-    getCategories().then(setCategoriePosts);
+  const getAllCategoryePost = () => {
+    getCategories().then(setCategoryPosts);
   };
 
-  const handleCategoryClick = (id) => {
+  const handleCategoryClick = (id, label) => {
     getPostsByCategory(id).then(setPosts);
+    setActiveFilter(`Category: ${label}`);
   };
 
   const getAllUsers = () => {
     getUsers().then(setAuthorPosts);
   };
-  const handleAuthorClick = (id) => {
+  const handleAuthorClick = (id, firstName, lastName) => {
     getPostsByUsers(id).then(setPosts);
+    setActiveFilter(`Author: ${`${firstName} ${lastName}`}`);
   };
 
   useEffect(() => {
     getAllPost();
-    getAllCategoriePost();
+    getAllCategoryePost();
     getAllUsers();
   }, []);
 
   return (
     <div>
+      <Button onClick={() => getAllPost()}>All Post</Button>
       <DropdownButton
         align="end"
         id="dropdown-item-button"
         title="Search by Category"
         className="drop-down-filter"
       >
-        {categoriePosts.map((category) => (
+        {categoryPosts.map((category) => (
           <div key={category.id}>
-            <Dropdown.Item as="button" onClick={() => handleCategoryClick(category.id)}>{category.label}</Dropdown.Item>
+            <Dropdown.Item as="button" onClick={() => handleCategoryClick(category.id, category.label)}>{category.label}</Dropdown.Item>
           </div>
         ))}
       </DropdownButton>
@@ -58,10 +64,11 @@ function Home() {
       >
         {authorPosts.map((author) => (
           <div key={author.id}>
-            <Dropdown.Item as="button" onClick={() => handleAuthorClick(author.id)}>{author.firstName} {author.lastName}</Dropdown.Item>
+            <Dropdown.Item as="button" onClick={() => handleAuthorClick(author.id, author.firstName, author.lastName)}>{author.firstName} {author.lastName}</Dropdown.Item>
           </div>
         ))}
       </DropdownButton>
+      <h2>Results for {activeFilter}</h2>
       { posts.length <= 0 ? (
         <p>No Posts Available</p>
       ) : (
