7a7f5677d60e8fefef89787df2441cac731aaae6
Alex Berka <alexanderberka@gmail.com>
2024-08-10 10:56:40 -0500

    Initial form functionality

--- api/categoryData.js
+++ api/categoryData.js
@@ -4,76 +4,76 @@ import clientCredentials from '../utils/data/client';
 const endpoint = clientCredentials.databaseURL;
 
 const getCategories = () => new Promise((resolve, reject) => {
-    fetch(`${endpoint}/categories`, {
-        method:'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
+  fetch(`${endpoint}/categories`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
     .then((response) => response.json())
     .then((data) => {
-        if (data) {
-          resolve(Object.values(data));
-        } else {
-          resolve([]);
-        }
-      }) 
-      .catch(reject);
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
 });
 
 const getSingleCategory = (id) => new Promise((resolve, reject) => {
-    fetch(`${endpoint}/categories/${id}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then(resolve)
-      .catch(reject);
-  });
+  fetch(`${endpoint}/categories/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
 
 const createCategory = (payload) => new Promise((resolve, reject) => {
-    fetch(`${endpoint}/categories`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload),
-    })
+  fetch(`${endpoint}/categories`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
     .then((response) => response.json())
     .then((data) => resolve(data))
     .catch(reject);
 });
 
 const updateCategory = (payload) => new Promise((resolve, reject) => {
-    fetch(`${endpoint}/categories/${payload.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    })
-      .then((response) => response.json())
-      .then(resolve)
-      .catch(reject);
-  });
+  fetch(`${endpoint}/categories/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
 
 const deleteCategory = (id) => new Promise((resolve, reject) => {
-    fetch(`${endpoint}/categories/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(resolve)
-      .catch(reject);
-  });
+  fetch(`${endpoint}/categories/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(resolve)
+    .catch(reject);
+});
 
 export {
-    getCategories,
-    getSingleCategory,
-    createCategory,
-    updateCategory,
-    deleteCategory,
-}
+  getCategories,
+  getSingleCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+};
