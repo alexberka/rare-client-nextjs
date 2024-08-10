2ca39f556957a9487fe9b11c52e08d868018a886
ursapictura <hsmithfreelance@gmail.com>
2024-08-10 13:46:37 -0500

    Added userPosts to userDetails

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -2,14 +2,18 @@ import { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 import { getUserById } from '../../api/userData';
+import { getPostsByUsers } from '../../api/postData';
+import PostCard from '../../components/PostCard';
 
 export default function UserDetails() {
   const [user, setUser] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
   const router = useRouter();
   const { id } = router.query;
 
   useEffect(() => {
     getUserById(id).then(setUser);
+    getPostsByUsers(id).then(setUserPosts);
   }, [id]);
 
   if (!user) {
@@ -17,25 +21,34 @@ export default function UserDetails() {
   }
 
   return (
-    <Table>
-      <tbody>
-        <tr>
-          <th scope="row">Full name</th>
-          <td>{user.firstName} {user.lastName}</td>
-        </tr>
-        <tr>
-          <th scope="row">Username</th>
-          <td>{user.username}</td>
-        </tr>
-        <tr>
-          <th scope="row">Creation date</th>
-          <td>{user.createdOn}</td>
-        </tr>
-        <tr>
-          <th scope="row">Bio</th>
-          <td>{user.bio}</td>
-        </tr>
-      </tbody>
-    </Table>
+    <>
+      <Table>
+        <tbody>
+          <tr>
+            <th scope="row">Full name</th>
+            <td>{user.firstName} {user.lastName}</td>
+          </tr>
+          <tr>
+            <th scope="row">Username</th>
+            <td>{user.username}</td>
+          </tr>
+          <tr>
+            <th scope="row">Creation date</th>
+            <td>{user.createdOn}</td>
+          </tr>
+          <tr>
+            <th scope="row">Bio</th>
+            <td>{user.bio}</td>
+          </tr>
+        </tbody>
+      </Table>
+      <div>
+        {userPosts.map((post) => (
+          // <PostCard key={post.id} postObj={post} onupdate={getPostsByUsers} />
+          <PostCard key={post.id} postObj={post} />
+        ))}
+      </div>
+
+    </>
   );
 }
