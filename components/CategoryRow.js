3f1514f100ab353606af8db17aaae209ee152950
ursapictura <hsmithfreelance@gmail.com>
2024-08-09 20:29:50 -0500

    API calls for categories

---
+++ components/CategoryRow.js
@@ -0,0 +1,22 @@
+// import { propTypes } from 'react-bootstrap/esm/Image';
+
+// export default function CategoryRow({ categoryObj, onUpdate }) {
+//   return (
+//     <tr>
+//       <th>{categoryObj.name}</th>
+//       <td>
+//         <button type="submit" className="btn btn-outline btn-accent btn-xs" onClick={removeThisCondition}>
+//           REMOVE
+//         </button>
+//       </td>
+//     </tr>
+//   );
+// }
+
+// CategoryRow.propTypes = {
+//   categoryObj: propTypes.shape({
+//     name: propTypes.string,
+//     firebaseKey: propTypes.string,
+//   }).isRequired,
+//   onUpdate: propTypes.func.isRequired,
+// };
--- pages/category-manager.js
+++ pages/category-manager.js
@@ -1,9 +1,41 @@
-import React from 'react';
+// import React, { useEffect } from 'react';
+// import { getCategories } from '../api/categoryData';
 
-export default function CategoryManager() {
-  return (
-    <div>
-      Category Manager
-    </div>
-  );
-}
+// export default function CategoryManager() {
+//   const [categories, setCategories] = useState([]);
+
+//   const getAllCategories = () => {
+//     getCategories().then(setCategories);
+//   }
+
+//   useEffect(() => {
+//     const sortCategoriess = categories.sort((a, b) => a.name.localeCompare(b.name));
+//   })
+//   return (
+//     <div className="overflow-x-auto med-con-table">
+//       <h2 className="title">Categories</h2>
+//       <table className="table table-xs">
+//         <thead>
+//           <tr>
+//             <th>Categories</th>
+//             <th aria-label="text" />
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {sortCategories.map((c) => ( // eslint-disable-line
+//             <CondRow key={c.id} categoryObj={c} onUpdate={getAllCategories} />
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
+
+// CategoryManager.propTypes = {
+//   categories: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string,
+//     }),
+//   ).isRequired,
+//   onUpdate: PropTypes.func.isRequired,
+// };
